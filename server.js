67c14e74ec292f9ab6315b65788d4c9e926f2f5d
Jérémie DEVIN <jeremie.devin@epsi.fr>
2018-03-09 09:39:24 +0100

    feat(update)

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
     host: "localhost",
     user: "integrationcontinue",
-    password: "integrationcontinue"
+    password: "integrationcontinue",
+    database: "integrationcontinue"
 });
 
 con.connect(function (err) {
     if (err) throw err;
-    console.log("Connected!");
+    con.query("SELECT * FROM utilisateurs", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+    });
 });
