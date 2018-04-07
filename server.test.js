82e3d1010f1188f20de6ea59b6919610b3f2b837
Jérémie DEVIN <jeremie.devin@epsi.fr>
2018-04-07 10:05:10 +0200

    feat(add & wip)

--- server.test.js
+++ server.test.js
@@ -1,3 +1,21 @@
 'use strict';
+require('./bootstrap');
+console.log('TEST UNITAIRE DE CONNEXION A LA BDD');
 
-console.log('test');
+var con = mysql.createConnection({
+    host: config.api.host,
+    port: config.api.port,
+    user: config.api.user,
+    password: config.api.password,
+    database: config.api.database,
+});
+
+con.connect(function (err) {
+    if (err) {
+        console.log('ECHEC');
+        throw err;
+    }
+    else{
+        console.log('REUSSITE');
+    }
+});
